08815c7a95c14b3d5e296e6023215ccbf64e355b
Pottifar <45870755+Pottifar@users.noreply.github.com>
2024-07-03 11:41:17 +0200

    Update kalkulator.c

--- C kode/kalkulator.c
+++ C kode/kalkulator.c
@@ -1,64 +1,54 @@
 #include <stdio.h>
 
-// Function prototypes
-int add(int num1, int num2);
-int sub(int num1, int num2);
-int div(int num1, int num2);
-int mul(int num1, int num2);
+double addNum(double num1, double num2){
+    double result = num1 + num2;
+    return result;
+}
 
-int main() {
+double sub(double num1, double num2){
+    double result = num1 - num2;
+    return result;
+}
+
+double div(double num1, double num2){
+    double result = num1 / num2;
+    return result;
+}
+
+double mul(double num1, double num2){
+    double result = num1 * num2;
+    return result;
+}
+
+int main(){
     int operation;
     int num1;
     int num2;
     int res;
 
-    printf("Choose operation:\nAdd: 1, Sub: 2, Div: 3, Mul: 4\n");
-    scanf("%d", &operation);
+    printf("Choose operaion:\nAdd: 1, Sub: 2, Div: 3, Mul: 4\n");
+    scanf("%d", operation);
 
     printf("Number 1:\n");
-    scanf("%d", &num1);
+    scanf("%d", num1);
 
     printf("Number 2:\n");
-    scanf("%d", &num2);
+    scanf("%d", num2);
 
-    switch (operation) {
+    switch (operation){
         case 1:
-            res = add(num1, num2);
+            res = addNum(num1, num2);
             break;
         case 2:
             res = sub(num1, num2);
             break;
         case 3:
-            if (num2 == 0) {
-                printf("Division by zero is not allowed.\n");
-                return 1;
-            }
             res = div(num1, num2);
             break;
         case 4:
             res = mul(num1, num2);
             break;
-        default:
-            printf("Invalid operation.\n");
-            return 1;
     }
 
-    printf("Result of calculation is: %d\n", res);
-    return 0;
-}
-
-int add(int num1, int num2) {
-    return num1 + num2;
-}
-
-int sub(int num1, int num2) {
-    return num1 - num2;
-}
-
-int div(int num1, int num2) {
-    return num1 / num2;
-}
-
-int mul(int num1, int num2) {
-    return num1 * num2;
+    printf("Result of calculation is:\n%d", res);
 }
